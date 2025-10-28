23974a8e5b96c77ad2adc6a176b01174d7708a50
jmt9454 <jmt9454@uncw.edu>
2025-10-28 19:16:02 -0400

    fixed hopefully

--- api/main.py
+++ api/main.py
@@ -22,7 +22,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],  # Allows all standard HTTP methods
     allow_headers=["*"],  # Allows all headers
 )
